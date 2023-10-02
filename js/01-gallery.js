d2172e92aa31c99bdbba0a680cfe066451d1e324
Oleksandr Maksymenko <machinery69@gmail.com>
2023-10-02 16:43:37 +0300

    finished gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,65 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+//import * as basicLightbox from "basicLightbox.min.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+createGallery();
+
+function createGallery() {
+  galleryItems.forEach((item) => {
+    const { preview, original, description } = item;
+
+    gallery.insertAdjacentHTML(
+      "beforeend",
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+    );
+  });
+}
+
+gallery.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  const link = event.target.dataset.source;
+  // console.dir(event.target);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <img src="${link}" width="800" height="600">
+  `);
+  instance.show();
+
+  if (instance.visible()) {
+    document.addEventListener("keydown", onEscPressed);
+  }
+
+  function onEscPressed(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", onEscPressed);
+    }
+    console.log(event.code);
+    console.log(instance.visible());
+  }
+}
+
+// if (instance.visible()) {
+//   document.addEventListener("keydown", onEscPressed);
+// }
+
+// function onEscPressed(event) {
+//   if (event.code === "Escape") i;
+//   console.log(event.code);
+// }
